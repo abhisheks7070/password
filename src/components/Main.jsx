8940464a0fa41b57c3fbfd7236d8eaa409967cc6
Abhishek <s.abhishek7070@gmail.com>
2024-09-18 15:36:28 +0530

    modified

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,39 +1,97 @@
 import React, { useState } from 'react'
 const Main = () => {
 
-    
-    const [verify, setVerify] = useState({ "pass": "", "long":false, "lc": false, "uc": false, "sc": false })
+
+    const [verify, setVerify] = useState({ "long": false, "lc": false, "uc": false, "sc": false, "num": false, "space": false })
 
 
     const handleChange = (event) => {
-        setVerify((values) => ({ ...values, ["pass"]: (event.target.value).trim() }))
+        const pass = (event.target.value).trim()
+        if (pass != "") {
 
-        ((verify.pass.length) >= 8) ? setVerify((values) => ({ ...values, ["long"]: true })) : setVerify((values) => ({ ...values, ["long"]: false }))
+            setVerify((values) => ({ ...values, ["long"]: ((pass.length) >= 8) ? true : false }))
+
+            const arr = pass.split("")
+
+            for (let i = 0; i <= arr.length - 1; i++) {
+                if (arr[i]==" ") {
+                    setVerify((values) => ({ ...values, ["space"]: true }))
+                    break
+                }
+
+                else{
+                    setVerify((values) => ({ ...values, ["space"]: false }))
+
+                }
+            }
+
+            for (let i = 0; i <= arr.length - 1; i++) {
+                if (arr[i] == arr[i].toLowerCase() && arr[i] != "@" && arr[i] != "_" && arr[i] != "#" && hasNumber(arr[i])==false) {
+                    setVerify((values) => ({ ...values, ["lc"]: true }))
+                    break
+                }
+
+                else{
+                    setVerify((values) => ({ ...values, ["lc"]: false }))
+
+                }
+            }
+
+            for (let i = 0; i <= arr.length - 1; i++) {
+                if (arr[i] == arr[i].toUpperCase() && arr[i] != "@" && arr[i] != "_" && arr[i] != "#" && hasNumber(arr[i])==false) {
+                    setVerify((values) => ({ ...values, ["uc"]: true }))
+                    break
+                }
+
+                else{
+                    setVerify((values) => ({ ...values, ["uc"]: false }))
+
+                }
+            }
+            for (let i = 0; i <= arr.length - 1; i++) {
+                if (arr[i] == "@" || arr[i] == "_" || arr[i] == "#") {
+                    setVerify((values) => ({ ...values, ["sc"]: true }))
+                    break
+                }
+                else{
+                    setVerify((values) => ({ ...values, ["sc"]: false }))
+
+                }
+            }
+            
+            function hasNumber(str) {
+                return /[0-9]/.test(str);
+            }
+            
+            
+            setVerify((values) => ({ ...values, ["num"]: hasNumber(pass) ? true : false }))
         
-        verify.pass.split().forEach((e) => {
-            (e == e.toLowerCase()) ? setVerify((values) => ({ ...values, ["lc"]: true })) : setVerify((values) => ({ ...values, ["lc"]: false }))
-                (e == e.toUpperCase()) ? setVerify((values) => ({ ...values, ["uc"]: true })) : setVerify((values) => ({ ...values, ["uc"]: false }))
-        })
+
+        }
+
+
+        else {
+            setVerify({ "long": false, "lc": false, "uc": false, "sc": false })
+        }
     }
 
-
-    
-            
-    console.log(verify.pass)
-    console.log(verify.long)
 
     return (
         <>
             <div className='mt-4 flex flex-col gap-2 justify-center items-center'>
-                <input className='border-black border-2 rounded-lg w-[30vw] h-16 placeholder:px-2' placeholder='Enter the password' onChange={handleChange} type="text" name="" id="" />
-                <div className='flex w-[29vw] justify-between font-extrabold text-xl'>
+                <input className='border-black border-2 rounded-lg w-[30vw] h-16 placeholder:px-2 text-2xl px-2' placeholder='Enter the password' onChange={handleChange} type="text" name="" id="" />
+                {
+                verify.space == false ? <div className='flex w-[29vw] justify-between font-extrabold text-lg'>
                     <span className={`${verify.lc == true ? "text-green-700" : "text-red-700"}`}>Lowercase</span>
                     <span className={`${verify.uc == true ? "text-green-700" : "text-red-700"}`}>Uppercase</span>
-                    <span>Special</span>
+                    <span className={`${verify.sc == true ? "text-green-700" : "text-red-700"}`}>Special_character</span>
+                    <span className={`${verify.num == true ? "text-green-700" : "text-red-700"}`}>Number</span>
                     <span className={`${verify.long == true ? "text-green-700" : "text-red-700"}`}>length</span>
-                </div>
-                <div className='mt-8'>
-                    Your password is:
+                </div> : <div>No Empty space are allowed</div>
+
+                }
+                <div className='mt-8 text-xl font-bold'>
+                    Your password is {(verify.lc && verify.uc && verify.sc && verify.long == true && verify.num == true && verify.space==false) ? "VALID" : "INVALID"}
                 </div>
             </div>
         </>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <div className='flex justify-between bg-green-500 items-center p-5'>
       <div>Abhishek</div>
-      <div><h2>Toast Popup</h2></div>
+      <div><h2>Password validator</h2></div>
       <div><a href="https://www.google.com" target="_blank">Link</a></div>
       {/* <a href="https://www.w3schools.com" target="_blank">Visit W3Schools.com!</a>  */}
     </div>
